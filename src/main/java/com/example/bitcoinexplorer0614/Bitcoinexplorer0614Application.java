0c187d39a1933ce326cbfa3f78e458851429f31b
XXX <1493427524@qq.com>
2019-06-15 07:53:46 +0800

    getRecentBlocks interface

--- src/main/java/com/example/bitcoinexplorer0614/Bitcoinexplorer0614Application.java
+++ src/main/java/com/example/bitcoinexplorer0614/Bitcoinexplorer0614Application.java
@@ -1,9 +1,11 @@
 package com.example.bitcoinexplorer0614;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.bitcoinexplorer0614.dao")
 public class Bitcoinexplorer0614Application {
 
 	public static void main(String[] args) {
