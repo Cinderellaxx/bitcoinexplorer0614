3444e4c3601366ca0ce8debaafddc4539addebff
XXX <1493427524@qq.com>
2019-06-14 20:47:43 +0800

    first init

---
+++ src/main/java/com/example/bitcoinexplorer0614/Bitcoinexplorer0614Application.java
@@ -0,0 +1,13 @@
+package com.example.bitcoinexplorer0614;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bitcoinexplorer0614Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bitcoinexplorer0614Application.class, args);
+	}
+
+}
