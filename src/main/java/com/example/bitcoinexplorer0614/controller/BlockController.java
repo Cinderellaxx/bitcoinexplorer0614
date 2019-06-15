d23f64df0efeff61b7075a7138b921654ccb8c39
XXX <1493427524@qq.com>
2019-06-15 08:54:01 +0800

    getByPrevBlcok interface and getByNextBlcok interface

--- src/main/java/com/example/bitcoinexplorer0614/controller/BlockController.java
+++ src/main/java/com/example/bitcoinexplorer0614/controller/BlockController.java
@@ -72,4 +72,38 @@ public class BlockController {
 
         return blockGetDTO;
     }
+
+    @GetMapping("/getByPrevBlcok")
+    public BlockGetDto getByPrevBlcok(@RequestParam String prevBlcok){
+        BlockGetDto blockGetDTO = new BlockGetDto();
+        blockGetDTO.setBlockhash("00000000000000000001ce5f88601a311f1c73c0073a15fe4e5956da7fbcd78b");
+        blockGetDTO.setHeight(580643);
+        blockGetDTO.setPrevBlcok("00000000000000000005ac7036789bfec28d230dff491f3382f6daf6523f5c44");
+        blockGetDTO.setNextBlock("00000000000000000024b3d4793dcbba032d3fc28a0d77a37d466b956fb68aa5");
+        blockGetDTO.setMerkleRoot("07ac3d1c827b5c3ef69a7341bbdb2bf72339139b5f9e7e782d1bc82265b17798");
+        blockGetDTO.setTime(new Date().getTime());
+        blockGetDTO.setFees(8766.38);
+        blockGetDTO.setTxSize((short) 2702);
+        blockGetDTO.setSize(1322496);
+        blockGetDTO.setDifficulty(7409399249090.25);
+
+        return blockGetDTO;
+    }
+
+    @GetMapping("/getByNextBlcok")
+    public BlockGetDto getByNextBlcok(@RequestParam String nextBlcok){
+        BlockGetDto blockGetDTO = new BlockGetDto();
+        blockGetDTO.setBlockhash("00000000000000000001ce5f88601a311f1c73c0073a15fe4e5956da7fbcd78b");
+        blockGetDTO.setHeight(580643);
+        blockGetDTO.setPrevBlcok("00000000000000000005ac7036789bfec28d230dff491f3382f6daf6523f5c44");
+        blockGetDTO.setNextBlock("00000000000000000024b3d4793dcbba032d3fc28a0d77a37d466b956fb68aa5");
+        blockGetDTO.setMerkleRoot("07ac3d1c827b5c3ef69a7341bbdb2bf72339139b5f9e7e782d1bc82265b17798");
+        blockGetDTO.setTime(new Date().getTime());
+        blockGetDTO.setFees(8766.38);
+        blockGetDTO.setTxSize((short) 2702);
+        blockGetDTO.setSize(1322496);
+        blockGetDTO.setDifficulty(7409399249090.25);
+
+        return blockGetDTO;
+    }
 }
