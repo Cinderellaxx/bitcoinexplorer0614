4c164aefa08cc8825d0016400b9ce1a873bc4a4f
XXX <1493427524@qq.com>
2019-06-17 21:26:50 +0800

    add username password url

--- src/main/java/com/example/bitcoinexplorer0614/api/impl/BitcoinJsonRpcApiImpl.java
+++ src/main/java/com/example/bitcoinexplorer0614/api/impl/BitcoinJsonRpcApiImpl.java
@@ -3,6 +3,7 @@ package com.example.bitcoinexplorer0614.api.impl;
 import com.alibaba.fastjson.JSONObject;
 import com.example.bitcoinexplorer0614.api.BitcoinJsonRpcApi;
 import com.googlecode.jsonrpc4j.JsonRpcHttpClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.net.MalformedURLException;
@@ -13,13 +14,15 @@ import java.util.HashMap;
 public class BitcoinJsonRpcApiImpl implements BitcoinJsonRpcApi {
     private JsonRpcHttpClient jsonRpcHttpClient;
 
-    public BitcoinJsonRpcApiImpl() throws MalformedURLException {
+    public BitcoinJsonRpcApiImpl(@Value("${bitcoin.jsonrpc.username}") String username,
+                                 @Value("${bitcoin.jsonrpc.password}") String password,
+                                 @Value("${bitcoin.jsonrpc.url}") String url) throws MalformedURLException {
         HashMap<String, String> headers = new HashMap<>();
-        String authStrOrig = String.format("%s:%s","xxx","123456");
+        String authStrOrig = String.format("%s:%s",username,password);
         String authStr = Base64.getEncoder().encodeToString(authStrOrig.getBytes());
         String authStrResult = String.format("Basic %s",authStr);
         headers.put("Authorization",authStrResult);
-        jsonRpcHttpClient = new JsonRpcHttpClient(new URL("http://localhost:18332"),headers);
+        jsonRpcHttpClient = new JsonRpcHttpClient(new URL(url),headers);
     }
 @Override
     public JSONObject getblockchaininfo() throws Throwable {
